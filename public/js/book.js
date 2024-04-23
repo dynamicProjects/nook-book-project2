6f8d7f774865e74557040b43ecc483a7896b707d
Ann Maria Jose <annmariajose1412@gmail.com>
2024-04-23 18:02:53 -0500

    Update book page overview

--- public/js/book.js
+++ public/js/book.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-  
+  let overviewText = document.getElementById("overviewText").innerText;
+  document.getElementById("sample").innerHTML = `<p>${overviewText}</p>`;
 });
