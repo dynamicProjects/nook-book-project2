b2d0f9a8001607918ae71476bdf229e59272494e
Ann Maria Jose <annmariajose1412@gmail.com>
2024-04-23 19:35:44 -0500

    Update book page purchase options

--- public/js/book.js
+++ public/js/book.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
-  let overviewText = document.getElementById("overviewText").innerText;
-  document.getElementById("sample").innerHTML = `<p>${overviewText}</p>`;
+  let purchaseOptions = document.getElementsByClassName("purchaseButtons");
+  purchaseOptions[0].classList.add("active");
+  changePurchaseTitleAndPrice(purchaseOptions[0]);
+
+  document.getElementById("overviewTextCard").innerHTML = `<p>${document.getElementById("overviewText").innerText}</p>`;
 });
+
+function changePurchaseTitleAndPrice(option) {
+  document.getElementById("purchaseOptionTitle").innerText = option.getElementsByClassName("option")[0].innerText;
+  document.getElementById("purchaseOptionPrice").innerText = option.getElementsByClassName("price")[0].innerText;
+}
+
+function changePurchaseOption(option) {
+  let purchaseOptions = document.getElementsByClassName("purchaseButtons");
+  Array.prototype.forEach.call(purchaseOptions, (element) => {
+    element.classList.remove("active");
+  });
+  option.classList.add("active");
+  changePurchaseTitleAndPrice(option);
+}
